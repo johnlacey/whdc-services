3024e80befab5a6301b409c5bd2c79d1a9e1039a
johnlacey <johnlacey@users.noreply.github.com>
2015-10-27 10:24:54 +1100

    Index.php is now the login page

--- services/backend/index.php
+++ services/backend/index.php
@@ -1,77 +1,38 @@
+<?php
+session_start(); 
+?> 
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Login</title>
 </head>
 
 <body>
-<table border="1">
-<tr>
-<th>Name</td>
-<th>Category</td>
-<th>Address</td>
-<th>City</td>
-<th>Web</td>
-<th>Edit</td>
-<th>Delete</td>
-</tr>
 <?php
-
-// 1. Create database connection 
-$dbhost = "localhost"; 
-$dbuser = "whdc_admin"; 
-$dbpass = "whdc_password"; 
-$dbname = "whdc_services"; 
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
-// Test if connection succeeded
-if (mysqli_connect_errno()){ 
-	die("Database connection failed: " . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")" ); 
-}
-?>
-<?php 
-$query = "SELECT service.name, service.category_id, service.id, service.address, service.city, service.postcode, service.phone, service.web, service.contact, category.id as catid, category.name as catname "; 
-$query .= "FROM service INNER JOIN category ON service.category_id=category.id ";
-$query .= " ORDER BY service.category_id ASC, service.name ASC;"; 
-$result = mysqli_query($connection, $query); 
-// Test if there was a query error 
-if (!$result){ 
-	die("Database query failed."); 
-}
-?> 
-<?php 
-// 3. Use returned data (if any) 
-while ($service = mysqli_fetch_assoc($result)){ 
-	$servicename = $service["name"];
-	$servicecategoryid = $service["category_id"]; 
-	$serviceid = $service["id"]; 
-	$serviceaddress = $service["address"];
-	$servicecity = $service["city"]; 
-	$servicepostcode = $service["postcode"]; 
-	$servicephone = $service["phone"]; 
-	$serviceweb = $service["web"]; 
-	$servicecontact = $service["contact"];
-	$servicecategoryname = $service["catname"];  
-?>
-<tr>
-<td><?php echo $servicename; ?></td>
-<td><?php echo $servicecategoryname; ?></td>
-<td><?php echo $serviceaddress; ?></td>
-<td><?php echo $servicecity; ?></td>
-<td><?php echo $serviceweb; ?></td>
-<td>
-<form action="edit.php" method="POST">
-<input name="id" type="hidden" value="<?php echo $serviceid; ?>">
-<input type="submit" value="Edit"></form></td>
-
-<td><form action="delete.php" method="POST">
-<input name="id" type="hidden" value="<?php echo $serviceid; ?>">
-<input type="submit" value="Delete"></form></td>
-</tr>
-<?php
+if (isset($_POST['submit'])){
+	if (htmlentities($_POST['username'])=="theusername"){ 
+		if (htmlentities($_POST['password'])=="thepassword"){ 
+			// Log is successful 
+			$_SESSION['loggedin'] = true; 
+			$_SESSION['username'] = "theusername"; 
+			// Redirect to overview page 
+			header('Location: overview.php'); 
+			exit; 
+		}
+	}
+echo "There was a problem with your username and/or password. Please try again."; 
+	
 } 
 ?>
-</table>
-<a href="add.php">Add a new service</a>
+
+<form action="" method="POST">
+<label for="username">Username:</label><input type="text" name="username" id="username" value="<?php if (isset($_POST['username'])){
+	echo $_POST['username']; 
+	} ?>"><br>
+<label for="password">Password:</label><input type="password" name="password" id="password"><br>
+<input type="submit" value="Log in" id="submit" name="submit"> 
+</form>
+
 </body>
 </html>
